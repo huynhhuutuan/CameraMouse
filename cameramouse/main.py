975f5ca5149bba05af3e1193a9aeb586b880244a
rbanalagay <rbanalagay@gmail.com>
2016-01-26 00:56:55 -0600

    basic tracking works

--- cameramouse/main.py
+++ cameramouse/main.py
@@ -3,32 +3,64 @@ import cv2
 import sys
 from PyQt5 import QtGui, QtCore, QtWidgets
 from threading import Thread
+from pymouse import PyMouse
 
 __author__ = 'rbanalagay'
 
-
+MOUSE = PyMouse()
 OFFSET = 200
 COLOR = (0, 0, 255)
 BOX_SIZE = 500
 CIRCLE_RADIUS = 30
 
 cap = cv2.VideoCapture(0)
-img = np.ones((1920, 1080, 3), np.uint8) * 255
 
 
 BOX_CORNERS = ((OFFSET, OFFSET), (OFFSET + BOX_SIZE, OFFSET),
                (OFFSET, OFFSET + BOX_SIZE),
                (OFFSET + BOX_SIZE, OFFSET + BOX_SIZE))
-for corner in BOX_CORNERS:
-    cv2.circle(img, corner, CIRCLE_RADIUS, COLOR, -1)
 
-KERNEL = np.ones((5, 5), np.uint8)
+
+KERNEL = np.ones((10, 10), np.uint8)
+
+
+def sort_corners(corners):
+    center = np.zeros(2)
+    for corner in corners:
+        center += np.array(corner)
+    center /= 4.0
+
+    top_points = []
+    bottom_points = []
+    for corner in corners:
+        x, y = corner
+        if y < center[1]:
+            bottom_points.append(corner)
+        else:
+            top_points.append(corner)
+
+    top_left = (top_points[0]
+                if top_points[0][0] < top_points[1][0]
+                else top_points[1])
+    top_right = (top_points[0]
+                 if top_points[0][0] >= top_points[1][0]
+                 else top_points[1])
+    bottom_left = (bottom_points[0]
+                   if bottom_points[0][0] < bottom_points[1][0]
+                   else bottom_points[1])
+    bottom_right = (bottom_points[0]
+                    if bottom_points[0][0] >= bottom_points[1][0]
+                    else bottom_points[1])
+    return np.array(top_left), np.array(top_right), np.array(bottom_left), np.array(bottom_right)
 
 
 def start_camera():
 
     while True:
         ret, frame = cap.read()
+        img = np.ones((1080, 1920, 3), np.uint8) * 255
+        for corner in BOX_CORNERS:
+            cv2.circle(img, corner, CIRCLE_RADIUS, COLOR, -1)
 
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -40,12 +72,30 @@ def start_camera():
         __, circles, _ = cv2.findContours(mask.copy(), cv2.RETR_LIST,
                                           cv2.CHAIN_APPROX_SIMPLE)
         circles = sorted(circles, key=cv2.contourArea, reverse=True)[:4]
+        corners = []
         for circle in circles:
             M = cv2.moments(circle)
-            centroid_x = int(M['m10']/M['m00'])
-            centroid_y = int(M['m01']/M['m00'])
+            centroid_x = M['m10']/M['m00']
+            centroid_y = M['m01']/M['m00']
+            corners.append((centroid_x, centroid_y))
             cv2.drawContours(frame, [circle], -1, (0, 255, 0), 3)
-            cv2.circle(frame, (centroid_x, centroid_y), 3, (0, 255, 255), -1)
+            cv2.circle(frame, (int(centroid_x), int(centroid_y)),
+                       3, (0, 255, 255), -1)
+
+        if len(circles) == 4:
+            sorted_corners = np.array(
+                np.vstack(sort_corners(corners)),
+                dtype='float32')
+            box_corners = np.array(
+                np.vstack([np.array(corner) for corner in BOX_CORNERS]),
+                dtype='float32')
+
+            transform_matrix = cv2.getPerspectiveTransform(sorted_corners, box_corners)
+            height, width, channels = frame.shape
+            pt = transform_matrix.dot(np.array([width / 2.0, height / 2.0, 1]))
+            cv2.circle(img, (int(pt[0]), int(1080 - pt[1])), 3, (0,0,0), -1)
+            # print(int(pt[0]), int(pt[1]))
+            # MOUSE.move(int(pt[0]), int(pt[1]))
 
 
         # Display the resulting frame
